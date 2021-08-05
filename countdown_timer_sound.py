4f65347ed08414e860c315ca09c2810e4da9c128
Exxxx <xelos.wot@gmail.com>
2021-08-05 13:31:26 -0400

    Add files via upload

---
+++ countdown_timer_sound.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+A countdown timer that loops.
+"""
+
+import keyboard
+import time
+import winsound
+
+soundfile = r"C:\Windows\Media\notify.wav"
+
+def countdown(t):
+        while t > 0:
+            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+            print(t)
+            t -= 1
+            time.sleep(1)
+            if keyboard.is_pressed("\\"):
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nResetting in "+sleepstr+" seconds!")
+                time.sleep(sleeps)
+                countdown(seconds)
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nNext in "+sleepstr+" seconds!")
+        winsound.PlaySound(soundfile,winsound.SND_ASYNC)
+        time.sleep(sleeps)
+        #winsound.Beep(37, 1000)
+        countdown(seconds)
+
+
+print("Press the backslash during countdown to reset.\n\nClose the program to select a new countdown length.\n\nHow many seconds to count down from?\nEnter an integer:")
+seconds = input()
+while not seconds.isdigit():
+    print("That wasn't an integer! Enter an integer:")
+    seconds = input()
+    
+print("Set the time between count downs.\nEnter an integer:")
+sleeps = input()
+while not sleeps.isdigit():
+    print("That wasn't an integer! Enter an integer:")
+    sleeps = input()
+
+
+
+
+seconds = int(seconds)
+sleeps = int(sleeps)
+sleepstr = str(sleeps)
+countdown(seconds)
